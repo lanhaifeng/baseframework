e24e988cbb3095ec7f4834fa9303838beb9e1790
lanhaifeng <lanhaifeng1991@163.com>
2022-03-06 11:30:28 +0800

    feat(SignalStudy): 进程通信-信号

--- src/main/java/com/feng/baseframework/autoconfig/ProcessorCommConfig.java
+++ src/main/java/com/feng/baseframework/autoconfig/ProcessorCommConfig.java
@@ -3,6 +3,7 @@ package com.feng.baseframework.autoconfig;
 import com.feng.baseframework.signal.SignalStudy;
 import org.springframework.context.annotation.Configuration;
 import sun.misc.Signal;
+import sun.misc.SignalHandler;
 
 import javax.annotation.PostConstruct;
 
@@ -17,8 +18,15 @@ import javax.annotation.PostConstruct;
 @Configuration
 public class ProcessorCommConfig {
 
+    /**
+     *  SignalHandler.SIG_IGN:The default signal handler
+     *  SignalHandler.SIG_DFL:Ignore the signal
+     *  SignalStudy:自定义处理行为
+     */
     @PostConstruct
     public void initSignal() {
         Signal.handle(new Signal("TERM"), new SignalStudy());
+        Signal.handle(new Signal("USR2"), SignalHandler.SIG_IGN);
+        Signal.handle(new Signal("CONT"), SignalHandler.SIG_DFL);
     }
 }
