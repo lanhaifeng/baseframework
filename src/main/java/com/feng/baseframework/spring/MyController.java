21aeef85b76e5a89bcdb9d817b9bf34c2da4fe46
lanhaifeng <lanhaifeng1991@163.com>
2022-05-04 22:25:29 +0800

    refactor(自定义HandlerMapping): 实现扩展springmvc

---
+++ src/main/java/com/feng/baseframework/spring/MyController.java
@@ -0,0 +1,42 @@
+package com.feng.baseframework.spring;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.feng.baseframework.constant.IpType;
+import com.feng.baseframework.model.User;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * baseframework
+ * 2022/5/3 18:19
+ * 测试自定义HandlerMapping的控制器
+ *
+ * @author lanhaifeng
+ * @since
+ **/
+@Component
+public class MyController {
+
+    @ResponseBody
+    public User about() {
+        User user = new User();
+        user.setName("about");
+        return user;
+    }
+
+    @ResponseBody
+    public JSONObject dict() {
+        JSONObject dict = new JSONObject();
+
+        JSONArray ipTypeJson = new JSONArray();
+        for (IpType value : IpType.values()) {
+            JSONObject type = new JSONObject();
+            type.put(value.name(), value.getValue());
+            ipTypeJson.add(type);
+        }
+
+        dict.put("IpType", ipTypeJson);
+        return dict;
+    }
+}
